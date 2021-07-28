6b78433add1b15a61c71a68c91d20178df89bcd3
Ar-Anik <67730427+Ar-Anik@users.noreply.github.com>
2021-07-29 02:05:53 +0600

    update

--- Test.py
+++ Test.py
@@ -1,3 +1,12 @@
-print("Aubdur Rob")
-print("\n")
-print("Anik")
+import matplotlib.pyplot as plt
+import seaborn
+import pandas
+
+tumor_size = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 150]
+
+malignant = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50]
+
+data = pandas.DataFrame(tumor_size, malignant)
+seaborn.regplot(x=tumor_size, y=malignant, data=data, scatter_kws={"s": 80})
+
+plt.show()
